d82ca1f7d9bcdf28bb77e5de9f17ee9b54eff89d
Pablo-Balaudo <balaudo.pablo@protonmail.com>
2020-08-16 01:35:30 -0300

    Test 1

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Data;
+
+namespace Funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Funcion(string funcion, int iteraciones, int tolerancia, int numero1, int numero2)
+            {
+                return (MismoSigno(numero1, numero2)) switch
+                {
+                    "Mismo signo"    => Biseccion(funcion, iteraciones, tolerancia, numero1, numero2).ToString(),
+                    "Distinto signo" => ("Volver a ingresar los intervalos"),
+                    _                => ("La raiz es " + MismoSigno(numero1, numero2)),
+                };
+            }
+
+            int Biseccion(string funcion, int iteraciones, int tolerancia, int numero1, int numero2)
+            {
+                while (true)
+                {
+
+                }
+
+                return 1;
+            }
+
+            /// <summary>
+            /// Devuelve 3 posibles strings, "Mismo signo", "Distinto signo", o la raiz en string 
+            /// </summary>
+            string MismoSigno(int numero1, int numero2)            
+            {
+                if (numero1 * numero2 > 0)
+                {
+                    return "Mismo signo";
+                }
+                else
+                {
+                    if (numero1 * numero2 < 0)
+                    {
+                        return "Distinto signo";
+                    }
+                    else
+                    {
+                        if (numero1 == 0)
+                        {
+                            return numero1.ToString();
+                        }
+                        else
+                        {
+                            return numero2.ToString();
+                        }
+                    }
+                }
+            }
+
+            int FormatearFuncion(string funcion, int numero)
+            {                
+                {
+                    string FuncionLimpia = funcion.Trim();
+                    FuncionLimpia = FuncionLimpia.Replace("X", numero.ToString());
+                    DataTable ResultadoNumero = new DataTable();
+                    return (int)ResultadoNumero.Compute(FuncionLimpia,"");
+                }
+            }
+            
+
+        }
+    }
+}

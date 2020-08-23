f6ba901522c010cb50b24f4afcd0ccc3dc9d76c8
Pablo-Balaudo <balaudo.pablo@protonmail.com>
2020-08-23 01:42:58 -0300

    Biseccion Lista

--- Logica/Unidad 1/Ejercicio1.cs
+++ Logica/Unidad 1/Ejercicio1.cs
@@ -8,23 +8,50 @@ namespace Logica.Unidad_1
     class Ejercicio1
     {
         string Funcion(string funcion, int iteraciones, int tolerancia, int numero1, int numero2)
-        //{
-        //    return (MismoSigno(numero1, numero2)) switch
-        //    {
-        //        "Mismo signo" => Biseccion(funcion, iteraciones, tolerancia, numero1, numero2).ToString(),
-        //        "Distinto signo" => ("Volver a ingresar los intervalos"),
-        //        _ => ("La raiz es " + MismoSigno(numero1, numero2)),
-        //    };
-        //}
+        {
+            switch (MismoSigno(numero1, numero2))
+            {
+                case "Mismo signo":
+                    return Biseccion(funcion, iteraciones, tolerancia, numero1, numero2).ToString();
+                case "Distinto signo": 
+                    return ("Volver a ingresar los intervalos");                   
+                default:
+                    return ("La raiz es " + MismoSigno(numero1, numero2));
+            }
+            //return (MismoSigno(numero1, numero2)) switch
+            //{
+            //    "Mismo signo" => Biseccion(funcion, iteraciones, tolerancia, numero1, numero2).ToString(),
+            //    "Distinto signo" => ("Volver a ingresar los intervalos"),
+            //    _ => ("La raiz es " + MismoSigno(numero1, numero2)),
+            //};
+        }
 
         int Biseccion(string funcion, int iteraciones, int tolerancia, int numero1, int numero2)
         {
+            int NoMeAcuerdoQueEra_XANT = 0;
+            int intentos = 0;
             while (true)
             {
-
-            }
-
-            return 1;
+                intentos++;
+                int mitad = (numero1 + numero2) / 2;
+                int error = Math.Abs(mitad - NoMeAcuerdoQueEra_XANT / mitad);
+                if (FormatearFuncion(funcion, mitad) < tolerancia || error < tolerancia || intentos >= iteraciones)
+                {
+                    return mitad;
+                }
+                else
+                {
+                    if (FormatearFuncion(funcion, numero1) * FormatearFuncion(funcion, numero2) > 0)
+                    {
+                        numero1 = mitad;
+                    }
+                    else
+                    {
+                        numero2 = mitad;
+                    }
+                    NoMeAcuerdoQueEra_XANT = mitad;
+                }
+            }         
         }
 
         /// <summary>

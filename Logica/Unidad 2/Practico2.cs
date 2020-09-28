a603d6d24d350e5005b145d9b5b50a18f9691cdb
DESKTOP-JP7M1VL\Cacu <francorossetti98@gmail.com>
2020-09-28 19:59:39 -0300

    limpio un poco

--- Logica/Unidad 2/Practico2.cs
+++ Logica/Unidad 2/Practico2.cs
@@ -7,47 +7,6 @@ namespace Logica.Unidad_2
     public class Practico2
     {
 
-        public static double Realizar_Determinante(double[,] matriz, int inc)
-        {
-            double resol = 0;
-            if (inc == 2)
-            {
-                return matriz[0, 0] * matriz[1, 1] - matriz[1, 0] * matriz[0, 1];
-            }
-            else
-            {
-                _ = new double[inc - 1, inc - 1];
-                for (int i = 0; i < inc; i++)
-                {
-                    double[,] aux = MatrizDeCofactores(matriz, inc, i);
-                    resol += Math.Pow(-1, i) * matriz[0, i] * Realizar_Determinante(aux, inc - 1);
-                }
-                return resol;
-            }
-        }
-
-        public static double[,] MatrizDeCofactores(double[,] matriz, int inc, int i)
-        {
-            double[,] matriz_aux = new double[inc - 1, inc - 1];
-            int x = 0;
-            int y = 0;
-            for (int j = 0; j < inc; j++)
-            {
-                for (int k = 0; k < inc; k++)
-                {
-                    if (j != 0 && k != i)
-                    {
-                        matriz_aux[x, y] = matriz[j, k];
-                        if (y < inc - 2)
-                        { y += 1; }
-                        else
-                        { x += 1; y = 0; };
-                    }
-                }
-            }
-            return matriz_aux;
-        }
-
         // -------------------------------------------------Gauss Jordan-------------------------------------------------------
 
         public static Resultado_TP2 GaussJordan(double[,] matriz, int dim)
@@ -86,13 +45,7 @@ namespace Logica.Unidad_2
         public static Resultado_TP2 GaussSeidel(double[,] matriz, int dim, int iter, double tole)
         {
             Resultado_TP2 result = new Resultado_TP2(true, "", dim, 0);
-            
-            if (Realizar_Determinante(matriz, dim) == 0)
-            {
-                result.Mensaje = "El sistema no converge en una resolución";
-                result.Ok = false;
-                return result;
-            }
+
             double[] s_iterado = new double[dim]; // seteo el vector S en 0
             for (int x = 0; x < dim; x++)
             {

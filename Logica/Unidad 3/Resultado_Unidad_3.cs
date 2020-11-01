1c8a45d1ba4c720e8e0c07ac6fa2da83a00cc0c0
Pablo Balaudo <Balaudo.Pablo@protonmail.com>
2020-11-01 18:08:24 -0300

    Added Regresion Lineal

---
+++ Logica/Unidad 3/Resultado_Unidad_3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logica.Unidad_3
+{
+    public class Resultado_Regresion_Lineal
+    {
+        public Resultado_Regresion_Lineal(bool resultado_correcto, string mensaje, double coeficioente, double a0, double a1)
+        {
+            Ok = resultado_correcto;
+            Mensaje = mensaje;
+            Coeficiente = coeficioente;
+            A0 = a0;
+            A1 = a1;
+        }
+        public bool Ok { get; set; }
+        public string Mensaje { get; set; }
+        public double Coeficiente { get; set; }
+        public double A0 { get; set; }
+        public double A1 { get; set; }
+    }
+}
--- Logica/Unidad 3/Unidad3.cs
+++ Logica/Unidad 3/Unidad3.cs
@@ -6,9 +6,48 @@ namespace Logica.Unidad_3
 {
     public class Unidad3
     {
-        public void RegresionLineal()
+        public static Resultado_Regresion_Lineal RegresionLineal
+            (double[] vector_x, double[] vector_y, double tolerancia, int n)
         {
-
+            double sum_x = 0;
+            double sum_y = 0;
+            double st = 0;
+            double sr = 0;
+            for (int i = 0; i < n; i++)
+            {
+                // Se obtienen las sumatorias de todos los componentes por columna
+                sum_x += vector_x[i];
+                sum_y += vector_y[i];
+            }
+            double sum_xy = sum_x * sum_y;
+            double sum_xx = sum_x * sum_x;
+            // Se encuentra Y
+            double a1 = ((n * sum_xy) - sum_xy) / ((n * sum_xx) - (sum_xx * sum_xx));
+            // Se encuentra X
+            double a0 = (sum_y / n) - ((a1 * sum_x) / n);
+            for (int i = 0; i < n-1; i++)
+            {                 
+                // Se calcula el coeficiente de la recta de mejor ajuste              
+                sr =+ Math.Pow((a1 * vector_x[i]) + a0 * vector_y[i], 2);
+                // Se calcula rl coeficiente de la recta promedio 
+                st =+ Math.Pow(((sum_y / n) - vector_y[i] ), 2);
+            }
+            double ajuste = Math.Sqrt((st - sr) / st) * 100;
+            bool resultado_correcto;
+            string mensaje;
+            // Si el ajuste no es aceptable
+            if (ajuste < tolerancia)
+            {
+                resultado_correcto = false;
+                mensaje = $"El ajuste no es aceptable ya que es menor a {tolerancia}";                
+            }
+            // Si el ajuste es aceptable
+            else
+            {
+                resultado_correcto = true;
+                mensaje = $"El ajuste es aceptable ya que es mayor o igual a {tolerancia}";
+            }
+            return new Resultado_Regresion_Lineal(resultado_correcto, mensaje, ajuste, a0, a1);
         }
 
         public static double[,] RegresionPolinomial(double[] vector_x, double[] vector_y, int n, int grado)

fa89a0a3fb58ef9e4d76dc5568da80309ea8fc72
Pablo Balaudo <Balaudo.Pablo@protonmail.com>
2020-11-02 06:53:27 -0300

    Finished Regresion Polinomial

--- Logica/Unidad 3/Resultado_Unidad_3.cs
+++ Logica/Unidad 3/Resultado_Unidad_3.cs
@@ -1,4 +1,6 @@
-﻿namespace Logica.Unidad_3
+﻿using Logica.Unidad_2;
+
+namespace Logica.Unidad_3
 {
     public class Resultado_Regresion_Lineal
     {
@@ -15,5 +17,24 @@
         public double Coeficiente { get; set; }
         public double A0 { get; set; }
         public double A1 { get; set; }
+    }
+
+    public class Regresion_Polinomial
+    {
+        public Regresion_Polinomial(bool resultado_correcto, string mensaje, double coeficiente, Resultado_TP2 resultado_sistema_de_ecuaciones)
+        {
+            Resultado_Correcto = resultado_correcto;
+            Mensaje = mensaje;
+            Coeficiente = coeficiente;
+            Resultado_Sistema_de_Ecuaciones = resultado_sistema_de_ecuaciones;
+        }
+        public bool Resultado_Correcto { get; set; }
+        public double Coeficiente { get; set; }
+        public string Mensaje { get; set; }
+        /* 
+         * Estado Ok, Mensaje y las Resoluciones del resultado del sistemas de ecuaciones
+         * se obtienen de los atributos de Resultado_Sistema_de_Ecuaciones
+         */
+        public Resultado_TP2 Resultado_Sistema_de_Ecuaciones { get; set; }
     }
 }
--- Logica/Unidad 3/Unidad3.cs
+++ Logica/Unidad 3/Unidad3.cs
@@ -1,34 +1,38 @@
-﻿using System;
+﻿using Logica.Unidad_2;
+using System;
 
 namespace Logica.Unidad_3
 {
     public class Unidad3
     {
         public static Resultado_Regresion_Lineal RegresionLineal
-            (double[] vector_x, double[] vector_y, double tolerancia, int n)
+            (double[] vector_x,
+            double[] vector_y,
+            double tolerancia)
         {
-            double sum_x = 0;
-            double sum_y = 0;
-            double st = 0;
-            double sr = 0;
-            for (int i = 0; i < n; i++)
+            int cantidad_de_pares_ordenados = vector_x.Length;
+            double sum_x = 0,
+                sum_y = 0,
+                st = 0,
+                sr = 0;
+            for (int i = 0; i < cantidad_de_pares_ordenados; i++)
             {
                 // Se obtienen las sumatorias de todos los componentes por columna
                 sum_x += vector_x[i];
                 sum_y += vector_y[i];
             }
-            double sum_xy = sum_x * sum_y;
-            double sum_xx = sum_x * sum_x;
+            double sum_xy = sum_x * sum_y,
+                sum_xx = sum_x * sum_x;
             // Se encuentra Y
-            double a1 = ((n * sum_xy) - sum_xy) / ((n * sum_xx) - (sum_xx * sum_xx));
-            // Se encuentra X
-            double a0 = (sum_y / n) - ((a1 * sum_x) / n);
-            for (int i = 0; i < n - 1; i++)
+            double a1 = ((cantidad_de_pares_ordenados * sum_xy) - sum_xy) / ((cantidad_de_pares_ordenados * sum_xx) - (sum_xx * sum_xx)),
+                // Se encuentra X
+                a0 = (sum_y / cantidad_de_pares_ordenados) - ((a1 * sum_x) / cantidad_de_pares_ordenados);
+            for (int i = 0; i < cantidad_de_pares_ordenados - 1; i++)
             {
                 // Se calcula el coeficiente de la recta de mejor ajuste              
                 sr = +Math.Pow((a1 * vector_x[i]) + a0 * vector_y[i], 2);
-                // Se calcula rl coeficiente de la recta promedio 
-                st = +Math.Pow(((sum_y / n) - vector_y[i]), 2);
+                // Se calcula el coeficiente de la recta promedio 
+                st = +Math.Pow(((sum_y / cantidad_de_pares_ordenados) - vector_y[i]), 2);
             }
             double ajuste = Math.Sqrt((st - sr) / st) * 100;
             bool resultado_correcto;
@@ -48,14 +52,23 @@ namespace Logica.Unidad_3
             return new Resultado_Regresion_Lineal(resultado_correcto, mensaje, ajuste, a0, a1);
         }
 
-        public static double[,] RegresionPolinomial(double[] vector_x, double[] vector_y, int n, int grado)
+        public static Regresion_Polinomial RegresionPolinomial
+            (double[] vector_x,
+            double[] vector_y,
+            int grado,
+            double tolerancia)
         {
-            double sum_x = 0, sum_y = 0;
+            int cantidad_de_pares_ordenados = vector_x.Length;
+            double sum_x = 0,
+                sum_y = 0,
+                ajuste = 0;
             double[,] matriz = new double[grado, grado + 1];
-            for (int i = 0; i < n; i++)
+            // Se calculan las sumatorias
+            for (int i = 0; i < cantidad_de_pares_ordenados; i++)
             {
                 sum_x += vector_x[i];
                 sum_y += vector_y[i];
+                // Se carga la matriz
                 for (int j = 0; j < grado; j++)
                 {
                     for (int k = 0; k < grado; k++)
@@ -65,7 +78,50 @@ namespace Logica.Unidad_3
                     matriz[j, grado] += vector_y[i] * (Math.Pow(vector_x[i], j));
                 }
             }
-            return matriz;
+            // Se resuelve el sistema de ecuaciones
+            Resultado_TP2 sistema_de_ecuaciones = Practico2.GaussJordan(matriz, grado + 1);
+            bool resultado_correcto = false;
+            string mensaje = "";
+            if (sistema_de_ecuaciones.Ok == true)
+            {
+                double sr = 0,
+                    st = 0,
+                    aux;
+                for (int i = 0; i < cantidad_de_pares_ordenados; i++)
+                {
+                    // Se calcula sr y st
+                    aux = vector_y[i] - sistema_de_ecuaciones.Resoluciones[0];
+                    for (int j = grado; j > 0; j++)
+                    {
+                        aux -=
+                            grado - 1 >= j ?
+                            Math.Pow(vector_x[i], j) * sistema_de_ecuaciones.Resoluciones[j] : 0;
+                    }
+                    sr += Math.Pow(aux, 2);
+                    st += Math.Pow(vector_y[i] - (sum_y / cantidad_de_pares_ordenados), 2);
+                    // Se calcula el coeficiente
+                    ajuste = ((st - sr) / st) * 100;
+                    // Si el ajuste no es aceptable
+                    if (ajuste < tolerancia)
+                    {
+                        resultado_correcto = false;
+                        mensaje = $"El ajuste no es aceptable ya que es menor a {tolerancia}";
+                    }
+                    // Si el ajuste es aceptable
+                    else
+                    {
+                        resultado_correcto = true;
+                        mensaje = $"El ajuste es aceptable ya que es mayor o igual a {tolerancia}";
+                    }
+                }
+            }
+            else
+            {
+                ajuste = 0;
+                mensaje = $"El sistema de ecuaciones fallo. Razon: {sistema_de_ecuaciones.Mensaje}";
+                resultado_correcto = false;
+            }
+            return new Regresion_Polinomial(resultado_correcto, mensaje, ajuste, sistema_de_ecuaciones);
         }
     }
 }
